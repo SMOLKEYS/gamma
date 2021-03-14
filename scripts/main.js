37b2a75cef76ff24478b5f788d77003bf7ba142c
SMOLKEYS.exe <75618732+SMOLKEYS@users.noreply.github.com>
2021-03-14 15:29:27 +0800

    Create main.js

---
+++ scripts/main.js
@@ -0,0 +1,5 @@
+UnitTypes.gamma.weapons.add(UnitTypes.corvus.weapons.get(0))
+UnitTypes.gamma.weapons.add(UnitTypes.toxopid.weapons.get(2))
+UnitTypes.gamma.weapons.add(UnitTypes.vela.weapons.get(0))
+UnitTypes.gamma.weapons.add(UnitTypes.poly.weapons.get(0))
+UnitTypes.gamma.weapons.add(UnitTypes.quad.weapons.get(0))
