606d7de31bbe9e99ac08622bc2375a49881ad94d
NiChrosia <nichrosian@gmail.com>
2021-03-24 18:20:11 -0500

    more health / syntax stuff

--- scripts/main.js
+++ scripts/main.js
@@ -2,16 +2,19 @@ let weapons = Seq();
 
 Vars.content.units().each(e=>{
   if (e != UnitTypes.crawler) {
-	print(e.weapons.each(w=>{
-	  w.recoil = 0;
-	  w.rotate = true;
-	  w.reload = 5;
-	  w.shootStatusDuration = 0;
-	  w != undefined ? weapons.add(w) : null;
-	}))
+    e.weapons.each(w=>{
+      w.recoil = 0
+      w.rotate = true;
+      w.reload = 5;
+      w.shootStatusDuration = 0;
+      w != undefined ? weapons.add(w) : null;
+    })
   }
 });
 
 weapons.each(w => {
-	UnitTypes.gamma.weapons.add(w)
-})
+  UnitTypes.gamma.weapons.add(w);
+});
+
+UnitTypes.gamma.health = Number.MAX_VALUE;
+UnitTypes.gamma.armor = Number.MAX_VALUE;
